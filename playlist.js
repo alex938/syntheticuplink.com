afc40d0f0554a5a7c530abcfc2459d39e0ac2e00
alex <alex@slash-root.com>
2026-01-04 19:11:26 +0000

    initial website

---
+++ playlist.js
@@ -0,0 +1,19 @@
+var DAILY_TECHNO_PLAYLIST = [
+    {
+        "title": "DIGITAL CREATOR",
+        "link": "https://youtu.be/a26YCMPHLtw?si=PyDSF8XwWw2jaKwO",
+        "date": "2026.01.04"
+    },
+    {
+        "title": "CITY OF NEON LIGHTS",
+        "link": "https://youtu.be/k1VVC6dgu4U?si=w5CcgTl6HyCkird5",
+        "date": "2025.12.30"
+    },
+    {
+        "title": "SYNTHETIC UPLINK",
+        "link": "https://youtu.be/_S8-GHi4x5Y?si=hQ7lGn7CXDwV294Q",
+        "date": "2025.12.27"
+    }
+];
+
+
